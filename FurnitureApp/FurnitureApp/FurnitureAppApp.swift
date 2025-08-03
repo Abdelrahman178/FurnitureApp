c5b9852fe6d270b2cf6e5b8e7b9d08aabd957e84
Abdelrahman-3amer <abdelrahmanamer252@gmail.com>
2025-08-03 17:39:15 +0300

    initial

---
+++ FurnitureApp/FurnitureApp/FurnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppApp.swift
+//  FurnitureApp
+//
+//  Created by Abdelrahman Amer on 03/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
