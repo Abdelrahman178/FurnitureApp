882fed7b122af87c73d94592c37728bfaea4aaf3
Abdelrahman-3amer <abdelrahmanamer252@gmail.com>
2025-08-04 17:04:54 +0300

    no message

--- FurnitureApp/FurnitureApp/Scenes/SplashScreen/SplashView.swift
+++ FurnitureApp/FurnitureApp/Scenes/SplashScreen/SplashView.swift
@@ -9,45 +9,43 @@ import SwiftUI
 
 struct SplashView: View {
     
+    @State private var isNavigating = false
+    
     var body: some View {
         
-        VStack() {
-            Image("SplashImage")
-                .resizable()
-                .frame( width: 428, height: 600)
-                .clipped()
-
-            VStack(alignment: .leading, spacing: 2) {
-                Text("Enjoy Your Furniture Renew")
-                    .font(.custom("Inter-Bold", size: 24))
-                    .foregroundColor(.black)
+        NavigationStack {
+            VStack() {
+                Image("SplashImage")
+                    .resizable()
+                    .frame(width: 428, height: 600)
+                    .clipped()
                 
-                Text("")
-                
-                Text("Browse through all categories and discover furniture ready for a second life in your dream home")
-                    .font(.custom("Inter-Regular", size: 16))
-                    .foregroundColor(.gray)
-
-
-                Spacer()
-                
-                Button(action: {
-
-                }) {
-                    Text("Get Started")
-                        .font(.custom("Inter-SemiBold", size: 18))
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color("SplashButtonColor"))
-                        .cornerRadius(40)
+                VStack(alignment: .leading, spacing: 2) {
+                    Text("Enjoy Your Furniture Renew")
+                        .font(.custom("Inter-Bold", size: 24))
+                        .foregroundColor(.black)
+                    
+                    Text("Browse through all categories and discover furniture ready for a second life in your dream home")
+                        .font(.custom("Inter-Regular", size: 16))
+                        .foregroundColor(.gray)
+                    
+                    Spacer()
+                    
+                    
+                    NavigationLink {
+                        
+                    } label: {
+                        Text("Get Started")
+                            .font(.custom("Inter-SemiBold", size: 18))
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color("SplashButtonColor"))
+                            .cornerRadius(40)
+                    }
+                    .padding(.horizontal)
                 }
             }
-            .padding()
-            .background(Color.white)
-            
-            Spacer()
         }
-        .edgesIgnoringSafeArea(.all)
     }
 }
