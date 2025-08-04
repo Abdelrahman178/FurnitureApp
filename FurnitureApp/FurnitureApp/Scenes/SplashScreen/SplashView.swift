dc11c9ee9876d0d8fdbee4e575f470f5df33ed15
Abdelrahman-3amer <abdelrahmanamer252@gmail.com>
2025-08-04 17:09:45 +0300

    Finalize Splash Screen

--- FurnitureApp/FurnitureApp/Scenes/SplashScreen/SplashView.swift
+++ FurnitureApp/FurnitureApp/Scenes/SplashScreen/SplashView.swift
@@ -9,43 +9,45 @@ import SwiftUI
 
 struct SplashView: View {
     
-    @State private var isNavigating = false
-    
     var body: some View {
         
-        NavigationStack {
-            VStack() {
-                Image("SplashImage")
-                    .resizable()
-                    .frame(width: 428, height: 600)
-                    .clipped()
+        VStack() {
+            Image("SplashImage")
+                .resizable()
+                .frame( width: 428, height: 600)
+                .clipped()
+
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Enjoy Your Furniture Renew")
+                    .font(.custom("Inter-Bold", size: 24))
+                    .foregroundColor(.black)
                 
-                VStack(alignment: .leading, spacing: 2) {
-                    Text("Enjoy Your Furniture Renew")
-                        .font(.custom("Inter-Bold", size: 24))
-                        .foregroundColor(.black)
-                    
-                    Text("Browse through all categories and discover furniture ready for a second life in your dream home")
-                        .font(.custom("Inter-Regular", size: 16))
-                        .foregroundColor(.gray)
-                    
-                    Spacer()
-                    
-                    
-                    NavigationLink {
-                        
-                    } label: {
-                        Text("Get Started")
-                            .font(.custom("Inter-SemiBold", size: 18))
-                            .foregroundColor(.white)
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color("SplashButtonColor"))
-                            .cornerRadius(40)
-                    }
-                    .padding(.horizontal)
+                Text("")
+                
+                Text("Browse through all categories and discover furniture ready for a second life in your dream home")
+                    .font(.custom("Inter-Regular", size: 16))
+                    .foregroundColor(.gray)
+
+
+                Spacer()
+                
+                Button(action: {
+
+                }) {
+                    Text("Get Started")
+                        .font(.custom("Inter-SemiBold", size: 18))
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color("SplashButtonColor"))
+                        .cornerRadius(40)
                 }
             }
+            .padding()
+            .background(Color.white)
+            
+            Spacer()
         }
+        .edgesIgnoringSafeArea(.all)
     }
 }
